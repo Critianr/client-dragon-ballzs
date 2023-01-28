7a1c9e314523436d6b9318f796b0a1dcdfd2c206
Critianr <calrod350@gmail.com>
2023-01-27 21:13:42 -0500

    config api v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,64 @@
-// import { useState } from 'react'
 import {useState, useEffect } from 'react'
 import './App.css'
 import { CardCharacter } from './components/Character/CardCharacter/CardCharacter'
 import img_title from './assets/dragonballLogo.png';
 function App() {
-  
 
+const [data, setData] = useState(null);
+const [character, setCharacter]= useState([]);
+const [search, setSearch]=useState([])
+
+const perGet=()=>{
+    fetch('./db.json')
+    .then(response => response.json())
+    .then(character=>setCharacter(character.characters))
+    .catch(error=>console.log(error));
+}
+
+const characterGet=()=>{
+    fetch('./db.json')
+    .then(response => response.json())
+    .then(data=>setData(data.characters.slice(0,5)))
+    .catch(error=>console.log(error));
+}
+const handleChange=e=>{
+    setSearch(e.target.value);
+    filtrar(e.target.value)
+
+    console.log("busqueda es : -> "+ e.target.value)
+}
+const filtrar=(terminoBusqueda)=>{
+    var resBusqueda=character.filter((elemento)=>{
+    if(elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+    || elemento.id.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+    ){
+    return elemento;
+    }
+    });
+    setData(resBusqueda);
+  }
+useEffect(()=>{
+    characterGet();
+    perGet();
+    },[]);
   return (
     <div className="App">
       <div className="main">
         <img className='img_title' src={img_title} alt="" />
         <div className='container-search'>
-        <input className='search-character' type="text" placeholder='Busca el personaje' />
+        <input className='search-character' type="text" 
+        placeholder='Busca el personaje'
+        value={search}
+        onChange={handleChange}
+
+        />
         </div>
-        {/* <div className='card-character'> */}
-        <CardCharacter />
+        <CardCharacter data={data} />
         <footer>
             <p>Copyright 2022</p>            
             <p>Si deseas colabboraciòn en extender la funcionalidad de la API</p><span>contactanos</span>
         </footer>
-        {/* </div> */}
-        {/* <div className='list-character'>
-        </div> */}
       </div>      
-      {/* <Card /> */}
-      {/* <div className="container searchContainer">
-<div className="header">
-    <img src="/assets/dragonballLogo.png" />
-</div>
-<div className="character-list">
-    <h1>Character List</h1>
-    <p>Type one of these character names, into the search box below. It will automatically, be converted to all
-        lowercase.</p>
-    <ul>
-        <li>Goku</li>
-        <li>Vegeta</li>
-        <li>Gohan</li>
-        <li>Trunks</li>
-        <li>Frieza</li>
-        <li>Cell</li>
-        <li>Buu</li>
-        <li>Beerus</li>
-        <li>Piccolo</li>
-        <li>Gotenks</li>
-        <li>Hit</li>
-        <li>Krilin</li>
-        <li>Jiren</li>
-        <li>Tien</li>
-        <li>Android 17</li>
-        <li>Android 18</li>
-        <li>Broly</li>
-        <li>Vegito</li>
-        <li>Gogeta</li>
-        <li>Goku Black</li>
-    </ul>
-</div>
-<div className="search">
-    <form className="dragonball-character">
-        <div className="container-search">
-            <label>
-                <h1>Dragonball Character Database</h1>
-            </label>
-            <label>
-                <p>Enter a characters name to fetch the user profile</p>
-            </label>
-            <input type="text" className="search-characters" placeholder="Search for a character" />
-        </div>
-    </form>
-
-</div>
-<div className="profile"></div>
-</div>       */}
     </div>
   )
 }
--- src/components/Character/CardCharacter/CardCharacter.jsx
+++ src/components/Character/CardCharacter/CardCharacter.jsx
@@ -1,19 +1,8 @@
 import {useState, useEffect } from 'react'
 import './CardCharacter.css'
 import './CardCharacter'
-// import data from '../../../db.json'; 
-// funciono
-
-function CardCharacter(){
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch('../../../db.json')
-      .then(response => response.json())
-      .then(data=> setData(data.characters))
-      .catch(error => console.log(error));
-  }, []);
-
-    return (
+const CardCharacter = ({data})=>{
+  return (
       <>
       {data && data.map(item => (
       <div key={item.id} className='container-card-character'>
@@ -23,7 +12,6 @@ function CardCharacter(){
         
         <li>Race: {item.race}</li>
         <li>Abilities: {item.abilities}</li>
-        
       </div>
        ))}
        </>
