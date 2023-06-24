f463dc818d8e91c40d90268673b8dbf511ef74e6
Itacgi <calrod350@gmail.com>
2023-06-24 22:37:31 +0200

    ajsutes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Header } from './components/Header/Header';
 
 
 function App() {
-  useClient(); 
+
 const [data, setData] = useState(null);
 const [character, setCharacter]= useState([]);
 const [search, setSearch]=useState([])
